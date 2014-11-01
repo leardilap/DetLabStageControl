40cc3676c939ff7e12565d43482b2275395fa823
Luis Ardila <leardilap@unal.edu.co>
2014-11-01 21:20:09 +0100

    Detector and Sample stages with 3 axis, independent Widgets

--- DetLabStage_v.1.py
+++ DetLabStage_v.1.py
@@ -1,19 +1,20 @@
 import sys
 from PyQt4 import QtGui, QtCore, uic
-import PyTango
 
-class Axis(QtGui.QMainWindow):
-	def __init__(self, Device, uiFile, Invert):
-		QtGui.QMainWindow.__init__(self)
+tangoEnable = False
+if tangoEnable:
+	import PyTango
+
+class Axis(QtGui.QWidget):
+	def __init__(self, Title, Device, uiFile, Invert):
+		QtGui.QWidget.__init__(self)
  
 		self.ui = uic.loadUi(uiFile)
-		self.ui.show()
-		
-		#####################################################
-		# Y SAMPLE
+		self.ui.show()		
 		
 		#Declaring Device
-		self.axis = PyTango.DeviceProxy(Device)
+		if tangoEnable:
+			self.axis = PyTango.DeviceProxy(Device)
 				
 		#Jogging Plus
 		self.connect(self.ui.JogPlus, QtCore.SIGNAL("pressed()"), self.JogPlus)
@@ -33,54 +34,132 @@ class Axis(QtGui.QMainWindow):
 		self.connect(self.ui.Scroll, QtCore.SIGNAL("valueChanged(int)"), self.UpdateDesiredPosScroll)
 		
 		#Updating State - Position
-		#self.timer = QtCore.QTimer()
-		#self.timer.start(100)
-		#self.connect(self.timer, QtCore.SIGNAL("timeout()"), self.UpdateState)
-		#self.connect(self.timer, QtCore.SIGNAL("timeout()"), self.CurrentPosition)
-		
-		#self.UpdateDesiredPos()
-		
-		self.ui.Scroll.setInvertedAppearance(Invert)
-		
-	      
+		self.timer = QtCore.QTimer()
+		self.timer.start(100)
+		self.connect(self.timer, QtCore.SIGNAL("timeout()"), self.UpdateState)
+		self.connect(self.timer, QtCore.SIGNAL("timeout()"), self.CurrentPosition)
+	
+		#####################
+		# Initializing Widget
+		self.UpdateDesiredPos()
+		self.ui.setWindowTitle(Title)
+		self.ui.Box.setTitle(Title)
+
+		if Invert:
+			if uiFile == "XWidget.ui":
+				self.ui.JogPlus.setGeometry(40,150,51,41)
+				self.ui.JogMinus.setGeometry(40,80,51,41)
+				self.ui.Scroll.setInvertedAppearance(not Invert)
+			elif (uiFile == "YWidget.ui") or (uiFile == "ZWidget.ui"):
+				self.ui.JogPlus.setGeometry(110,50,51,41)
+				self.ui.JogMinus.setGeometry(160,50,51,41)
+				self.ui.Scroll.setInvertedAppearance(Invert)
+		else:
+			if uiFile == "XWidget.ui":
+				self.ui.JogPlus.setGeometry(40,80,51,41)
+				self.ui.JogMinus.setGeometry(40,150,51,41)
+				self.ui.Scroll.setInvertedAppearance(not Invert)
+			elif (uiFile == "YWidget.ui") or (uiFile == "ZWidget.ui"):
+				self.ui.JogPlus.setGeometry(160,50,51,41)
+				self.ui.JogMinus.setGeometry(110,50,51,41)
+				self.ui.Scroll.setInvertedAppearance(Invert)
+	   
 	def JogPlus(self):
-		self.axis.forward()
+		if tangoEnable:
+			self.axis.forward()
+		else:
+			self.ui.StatusLabel.setText("JogPlus")
 		
 	def JogMinus(self):
-		self.axis.backward()
+		if tangoEnable:
+			self.axis.backward()
+		else:
+			self.ui.StatusLabel.setText("JogMinus")
 		
 	def Home(self):
-		self.axis.initializeReferencePosition()	
+		if tangoEnable:
+			self.axis.initializeReferencePosition()	
+		else:
+			self.ui.StatusLabel.setText("Home")
 	    
 	def Stop(self):
-		self.axis.stop()
+		if tangoEnable:
+			self.axis.stop()
+		else:
+			self.ui.StatusLabel.setText("")
 		self.UpdateDesiredPos()
 		
 	def Move(self):
 		pos = self.ui.DesirePos.value()
-		self.axis.position = pos			
+		if tangoEnable:
+			self.axis.position = pos	
+		else:
+			self.ui.StatusLabel.setText(str(pos))		
 		
 	def UpdateDesiredPosScroll(self):
 		pos = self.ui.Scroll.value()
 		self.ui.DesirePos.setValue(pos)
 	
 	def UpdateDesiredPos(self):
-		pos = self.axis.position
-		self.ui.DesirePos.setValue(pos)
-		self.ui.DesirePos.setValue(pos)	
+		if tangoEnable:
+			pos = self.axis.position
+			self.ui.DesirePos.setValue(pos)
 	     
 	def CurrentPosition(self):
-		pos = self.axis.position
-		self.ui.CurrentPos.display(pos)
+		if tangoEnable:
+			pos = self.axis.position
+			self.ui.CurrentPos.display(pos)
 	    
 	def UpdateState(self):
-		state = self.axis.state()
-		self.ui.StatusLabel.setText(str(state))
+		if tangoEnable:
+			state = self.axis.state()
+			self.ui.StatusLabel.setText(str(state))
 	
 if __name__ == "__main__":
 	app = QtGui.QApplication(sys.argv)
-	uiFile = "YWidget.ui" #"GUI_DetLabStage.ui"
-	Device = "anka/motor_detlab/ysample"
-	Invert = True
-	win = Axis(Device, uiFile, Invert)
+	
+	#############################################################
+	# X Sample
+	XS_Title = "X Sample"
+	XS_Device = "anka/motor_detlab/xsample"
+	XS_uiFile = "XWidget.ui"
+	XS_Invert = True
+	XSample = Axis(XS_Title, XS_Device, XS_uiFile, XS_Invert)
+	
+	# Y Sample
+	YS_Title = "Y Sample"
+	YS_Device = "anka/motor_detlab/ysample"
+	YS_uiFile = "YWidget.ui"
+	YS_Invert = True
+	YSample = Axis(YS_Title, YS_Device, YS_uiFile, YS_Invert)
+	
+	# Z Sample
+	ZS_Title = "Z Sample"
+	ZS_Device = "anka/motor_detlab/zsample"
+	ZS_uiFile = "ZWidget.ui"
+	ZS_Invert = True
+	ZSample = Axis(ZS_Title, ZS_Device, ZS_uiFile, ZS_Invert)
+
+	##############################################################
+	# X Detector
+	XD_Title = "X Detector"
+	XD_Device = "anka/motor_detlab/xdet"
+	XD_uiFile = "XWidget.ui"
+	XD_Invert = False
+	XDetector = Axis(XD_Title, XD_Device, XD_uiFile, XD_Invert)
+
+	# Y Detector
+	YD_Title = "Y Detector"
+	YD_Device = "anka/motor_detlab/ydet"
+	YD_uiFile = "YWidget.ui"
+	YD_Invert = False
+	YDetector = Axis(YD_Title, YD_Device, YD_uiFile, YD_Invert)
+
+	# Z Detector
+	ZD_Title = "Z Detector"
+	ZD_Device = "anka/motor_detlab/zdet"
+	ZD_uiFile = "ZWidget.ui"
+	ZD_Invert = False
+	ZDetector = Axis(ZD_Title, ZD_Device, ZD_uiFile, ZD_Invert)
+
 	sys.exit(app.exec_())
